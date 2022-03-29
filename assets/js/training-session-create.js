eefc2f63ba5b33e5436067365c35b852f0812548
S8A <samuelochoap@gmail.com>
2022-03-29 19:36:06 -0400

    Remove console.log line

--- assets/js/training-session-create.js
+++ assets/js/training-session-create.js
@@ -58,7 +58,6 @@ window.addEventListener('load', function () {
         } else {
           // If the number of valid exercises differs from the number
           // of row items (logically by being lower), show error message
-          console.log(exercises);
           statusText = 
               'Uno o varios ítems de ejercicio contienen datos inválidos. '
               + 'Verifique todos los campos.';
