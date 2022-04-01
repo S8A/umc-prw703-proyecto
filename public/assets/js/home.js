ef030aafd8b328b6f64f786b41cd3f533828b643
S8A <samuelochoap@gmail.com>
2022-04-01 12:34:56 -0400

    Adapt home.js to Firebase

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,13 +1,18 @@
-import * as utils from '/assets/js/utils.js';
+import * as utils from './utils.js';
+import { auth, getUserDoc } from './firebase.js';
 
 
-function setUpSignedInActionCall(account) {
-  /* Replace action call paragraph with customized text for the
-  signed-in account. */
+
+/**
+ * Replace action call paragraph with customized text for the signed-in
+ * user.
+ * @param {string} firstName - First name of the user.
+ */
+function setUpSignedInActionCall(firstName) {
 
   let actionCall = document.querySelector('#action-call');
   actionCall.textContent =
-      'Le damos la bienvenida, ' + account.firstName + '. Puede revisar su ';
+      'Le damos la bienvenida, ' + firstName + '. Puede revisar su ';
 
   let trainingLogLink = document.createElement('a');
   trainingLogLink.href = '/historial/';
@@ -28,13 +33,41 @@ function setUpSignedInActionCall(account) {
 
 
 window.addEventListener('load', function () {
-  let signedInAccount = utils.getSignedInAccount();
+  // Set up authentication state observer
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      // If user is signed in, get user data document
+      getUserDoc(user)
+      .then((snapshot, options) => {
+        // After getting the user's data document, set up header and
+        // customized action call
+        const data = snapshot.data(options);
+        utils.setUpSignedInHeader(data.name.first, data.name.last);
+        setUpSignedInActionCall(data.name.first);
+      })
+      .catch((error) => {
+        // If the user's data document couldn't be found, show error message
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(`${errorCode}: ${errorMessage}`);
 
-  // If signed-in, set up header & customized action call
-  if (signedInAccount) {
-    utils.setUpSignedInHeader(signedInAccount);
-    setUpSignedInActionCall(signedInAccount);
-  }
+        let statusText = '';
+
+        if (errorCode === 'auth/not-found') {
+          statusText =
+              'El usuario no tiene datos registrados en el sistema. '
+              + 'Comuníquese con el administrador: samuelochoap@gmail.com';
+        } else {
+          statusText =
+              'Error inesperado al tratar de consultar los datos del usuario. '
+              + `Código: ${errorCode}`;
+        }
+
+        utils.clearStatusMessages();
+        utils.addStatusMessage('alert-danger', [statusText]);
+      });
+    }
+  });
 
   // Add pending status message to page
   utils.addPendingStatusMessage();
