dcdace00004b4d948925cb4fb50a97314a44d9fb
S8A <samuelochoap@gmail.com>
2022-04-02 19:57:27 -0400

    Adapt module to Firestore & new -form-common.js

--- public/assets/js/training-session-create.js
+++ public/assets/js/training-session-create.js
@@ -1,79 +1,141 @@
-import * as utils from '/assets/js/utils.js';
-import * as common from '/assets/js/training-session-form-common.js';
+import * as common from './training-session-form-common.js';
+import * as utils from './utils.js';
+import { auth, createTrainingSession } from './firebase.js';
+
+
+/**
+ * Add empty form text to the given container.
+ * @param {HTMLElement} container - Container to which the text will be added.
+ */
+ function addEmptyFormText(container) {
+  const emptyText = document.createElement('p');
+  emptyText.textContent =
+      'No se ha podido cargar el formulario para crear un nuevo registro.';
+  container.appendChild(emptyText);
+}
 
 
 window.addEventListener('load', function () {
-  let signedInAccount = utils.getSignedInAccount();
+  // Form container
+  const container = document.querySelector('#training-session-form-container');
 
-  if (!signedInAccount) {
-    // If not signed-in, set pending info message and redirect to sign-in
-    let text = 'Inicie sesión para registrar una sesión de entrenamiento.';
-    utils.setPendingStatusMessage('alert-info', [text]);
-    window.location.assign('/iniciar-sesion.html?next=/historial/crear.html');
-    return;
-  } else {
-    // If signed-in, set up signed-in header
-    utils.setUpSignedInHeader(signedInAccount);
+  // Form parameters
+  const mainTitleText = 'Registrar una nueva sesión de entrenamiento';
+  const formId = 'create-form';
+  const formLabel = 'Registrar datos de la sesión de entrenamiento';
+  const submitButtonText = 'Crear registro';
 
-    // Get query params
-    const params = utils.getQueryParams();
+  // Get query params
+  const params = utils.getQueryParams();
 
-    // Construct create form page
-    let mainTitleText = 'Registrar una nueva sesión de entrenamiento';
-    let formId = 'create-form';
-    let formLabel = 'Registrar datos de la sesión de entrenamiento';
-    let submitButtonText = 'Crear registro';
+  // Set up authentication state observer
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      // If user is signed in, set up signed-in header
+      utils.setUpSignedInHeader(user);
 
-    common.constructTrainingSessionForm(
-        mainTitleText, formId, formLabel, submitButtonText);
+      // Construct create form page
+      common.constructTrainingSessionForm(
+          mainTitleText, formId, formLabel, submitButtonText);
 
-    // Add initial empty exercise item to table
-    common.addExercise();
+      // Add initial empty exercise item row to table
+      common.addExerciseItemRow();
 
-    // Get form element and add event listener for submission
-    let form = document.getElementById(formId);
+      // Get form element and add event listener for submission
+      const form = document.getElementById(formId);
 
-    form.addEventListener('submit', function (event) {
-      event.preventDefault();
-      event.stopPropagation();
+      form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
 
-      let statusText = '';
-      let statusType = 'alert-danger';
+        if (form.checkValidity()) {
+          // If form is valid, extract form data and create TrainingSession
+          const trainingSession = common.extractTrainingSessionData();
 
-      if (form.checkValidity()) {
-        // If form is valid, extract data from form fields and try to
-        // create training session
-        let data = common.extractFormData(form);
-        let session = utils.createTrainingSession(data);
+          if (trainingSession.isValid()) {
+            // If the training session is valid, try to add it as a document
+            // to the user's trainingSessions subcollection
+            createTrainingSession(user.uid, trainingSession)
+            .then((ref) => {
+              // If the training session is created successfully, set
+              // pending success message
+              utils.setPendingStatusMessage(
+                  'success',
+                  ['Sesión de entrenamiento registrada exitosamente.']
+              );
 
-        if (session) {
-          // If the training session was created, set pending success message
-          statusType = 'alert-success';
-          statusText = 'Sesión de entrenamiento registrada exitosamente.'
-          utils.setPendingStatusMessage(statusType, [statusText]);
+              // Redirect to detail page of the newly created training session
+              window.location.assign('/historial/detalle.html?id=' + ref.id);
+            })
+            .catch((error) => {
+              // If the training session could not be created, show
+              // appropriate error message
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              console.log(`${errorCode}: ${errorMessage}`);
 
-          // Redirect to detail page of the newly created training session
-          // and end event handler execution
-          window.location.assign('/historial/detalle.html?id=' + session.id);
-          return;
+              let statusText = '';
+
+              if (errorCode === 'deadline-exceeded') {
+                statusText =
+                    'El tiempo de respuesta de la solicitud expiró. '
+                    + 'Intente de nuevo más tarde.';
+              } else if (errorCode === 'permission-denied') {
+                statusText = 'No tiene permiso de realizar la operación.';
+              } else if (errorCode === 'unavailable') {
+                statusText =
+                    'Servicio temporalmente no disponible. '
+                    + 'Intente de nuevo más tarde';
+              } else {
+                statusText = `Error inesperado. Código: ${errorCode}`;
+              }
+
+              utils.clearStatusMessages();
+              utils.addStatusMessage('alert-danger', [statusText]);
+            });
+          } else {
+            // If the training session is invalid, then some field must
+            // have an invalid value somehow
+            utils.clearStatusMessages();
+            utils.addStatusMessage(
+                'alert-danger',
+                ['Uno o varios campos contienen datos inválidos. '
+                + 'Verifique e intente de nuevo.']
+            );
+          }
         } else {
-          // If the number of valid exercises differs from the number
-          // of row items (logically by being lower), show error message
-          statusText =
-              'Uno o varios ítems de ejercicio contienen datos inválidos. '
-              + 'Verifique todos los campos.';
+          // If the form is not valid, show error message
+          utils.clearStatusMessages();
+          utils.addStatusMessage(
+              'alert-danger',
+              ['Corrija los errores en los datos ingresados.']
+          );
         }
-      } else {
-        // If the form is not valid
-        statusText = 'Corrija los errores en los datos ingresados.';
+
+        // Add .was-validated to form if it wasn't already
+        form.classList.add('was-validated');
+      });
+    } else {
+      // If the user is signed-out, add info message indicating the
+      // user to sign in
+      utils.addStatusMessage(
+        'alert-info',
+        ['Inicie sesión para registrar una nueva sesión de entrenamiento.']
+      );
+
+      // Remove form if present
+      const form = document.getElementById(formId);
+      if (form) {
+        form.remove();
       }
 
-      // Add .was-validated to form if it wasn't already
-      form.classList.add('was-validated');
+      // Add empty results text if it isn't present
+      if (!document.querySelector('p#empty-text')) {
+        addEmptyFormText(container);
+      }
+    }
+  });
 
-      // Clear status area and add appropriate error message
-      utils.clearStatusMessages();
-      utils.addStatusMessage(statusType, [statusText]);
-    });
-  }
+  // Add pending status message to page
+  utils.addPendingStatusMessage();
 });
