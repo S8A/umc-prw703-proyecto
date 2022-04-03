36c546ba512207f3ed7b525e31f2b8a64a102463
S8A <samuelochoap@gmail.com>
2022-04-03 16:03:27 -0400

    Adapt edit form JS to Firestore

--- public/assets/js/training-session-edit.js
+++ public/assets/js/training-session-edit.js
@@ -1,101 +1,211 @@
-import * as utils from '/assets/js/utils.js';
-import * as common from '/assets/js/training-session-form-common.js';
+import * as common from './training-session-form-common.js';
+import * as utils from './utils.js';
+import { auth, getTrainingSession, updateTrainingSession } from './firebase.js';
+
+
+/**
+ * Add empty form text to the given container.
+ * @param {HTMLElement} container - Container to which the text will be added.
+ */
+ function addEmptyFormText(container) {
+  const emptyText = document.createElement('p');
+  emptyText.textContent =
+      'No se ha podido cargar el formulario para modificar la sesión de'
+      + 'entrenamiento solicitada.';
+  container.appendChild(emptyText);
+}
 
 
 window.addEventListener('load', function () {
-  let signedInAccount = utils.getSignedInAccount();
+  // Form container
+  const container = document.querySelector('#training-session-form-container');
 
-  if (!signedInAccount) {
-    // If not signed-in, set pending info message and redirect to sign-in
-    let text = 'Inicie sesión para gestionar sus sesiones de entrenamiento.';
-    utils.setPendingStatusMessage('alert-info', [text]);
-    window.location.assign('/iniciar-sesion.html?next=/historial/');
+  // Form parameters
+  let mainTitleText = 'Modificar sesión de entrenamiento: ';
+  const formId = 'edit-form';
+  const formLabel = 'Modificar datos de la sesión de entrenamiento';
+  const submitButtonText = 'Guardar cambios';
+
+  // Get query params
+  const params = utils.getQueryParams();
+
+  // Training session ID
+  const id = params.id;
+
+  if (!id) {
+    // If ID parameter is not set, redirect to training log page
+    window.location.assign('/historial/');
     return;
-  } else {
-    // If signed-in, set up signed-in header
-    utils.setUpSignedInHeader(signedInAccount);
+  }
 
-    // Get query params
-    const params = utils.getQueryParams();
+  // Set up authentication state observer
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      // If user is signed in, set up signed-in header
+      utils.setUpSignedInHeader(user);
 
-    // Training session ID
-    const id = Number(params.id);
+      // Try to get the user's requested training session
+      getTrainingSession(user.uid, id)
+      .then((trainingSession) => {
+        // Construct edit form page
+        mainTitleText += trainingSession.fullTitle;
+        common.constructTrainingSessionForm(
+            mainTitleText,
+            formId,
+            formLabel,
+            submitButtonText,
+            trainingSession
+        );
 
-    // If no valid ID parameter set, redirect to training log page
-    if (!Number.isInteger(id) || id <= 0) {
-      window.location.assign('/historial/');
-      return;
-    }
+        // Get form element and add event listener for submission
+        const form = document.getElementById(formId);
 
-    // Try to get training session with the given ID
-    let session = utils.getTrainingSession(id);
+        form.addEventListener('submit', function (event) {
+          event.preventDefault();
+          event.stopPropagation();
 
-    if (!session) {
-      // If the training session was not found, add error status message
-      let text = 'La sesión de entrenamiento solicitada no existe o '
-      + 'no pertenece a su cuenta.';
-      utils.clearStatusMessages();
-      utils.addStatusMessage('alert-danger', [text]);
-    } else {
-      // If the training session was found, construct edit form page with
-      // the training session's data
-      let fullTitle = utils.getTrainingSessionFullTitle(session);
-      let mainTitleText = 'Modificar sesión de entrenamiento: ' + fullTitle;
-      let formId = 'edit-form';
-      let formLabel = 'Modificar datos de la sesión de entrenamiento';
-      let submitButtonText = 'Guardar cambios';
+          if (form.checkValidity()) {
+            // If form is valid, extract form data and create TrainingSession
+            const trainingSession = common.extractTrainingSessionData();
 
-      common.constructTrainingSessionForm(
-          mainTitleText, formId, formLabel, submitButtonText, session);
+            // Try to add the training session as a document to the
+            // user's trainingSessions subcollection
+            updateTrainingSession(user.uid, id, trainingSession)
+            .then(() => {
+              // If the training session is created successfully, set
+              // pending success message
+              utils.setPendingStatusMessage(
+                  'alert-success',
+                  ['Sesión de entrenamiento modificada exitosamente.']
+              );
 
-      // Set page title
-      document.title = mainTitleText + ' ' + utils.NDASH + ' 8A Training';
+              // Redirect to detail page of the training session
+              window.location.assign('/historial/detalle.html?id=' + id);
+            })
+            .catch((error) => {
+              // If the training session could not be updated, show
+              // appropriate error message
+              console.log(error);
 
-      // Get form element and add event listener for submission
-      const form = document.getElementById(formId);
+              let statusText = '';
 
-      form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        event.stopPropagation();
+              if (error === 'user-doc-does-not-exist') {
+                statusText =
+                    'El usuario no tiene datos registrados en la base de datos.'
+                    + 'Comuníquese con el administrador: '
+                    + 'samuelochoap@gmail.com';
+              } else if (error === 'invalid-training-session') {
+                statusText =
+                    'La sesión de entrenamiento contiene datos inválidos. '
+                    + 'Verifique los datos ingresados.';
+              } else if (error === 'training-session-not-found') {
+                statusText =
+                    'La sesión de entrenamiento fue eliminada antes de '
+                    + 'completar la modificación de los datos. '
+                    + 'Regrese al historial de entrenamiento.';
+              } else if (error === 'deadline-exceeded') {
+                statusText =
+                    'El tiempo de respuesta de la solicitud expiró. '
+                    + 'Intente de nuevo más tarde.';
+              } else if (error === 'permission-denied') {
+                statusText = 'No tiene permiso de realizar la operación.';
+              } else if (error === 'unavailable') {
+                statusText =
+                    'Servicio temporalmente no disponible. '
+                    + 'Intente de nuevo más tarde';
+              } else {
+                statusText = `Error inesperado. Código: ${error}`;
+              }
 
-        let statusText = '';
-        let statusType = 'alert-danger';
+              utils.clearStatusMessages();
+              utils.addStatusMessage('alert-danger', [statusText]);
 
-        if (form.checkValidity()) {
-          // If form is valid, extract data from form fields and try to
-          // update training session
-          let data = common.extractFormData(form);
-          let session = utils.updateTrainingSession(id, data);
-
-          if (session) {
-            // If the training session was updated, set pending success message
-            statusType = 'alert-success';
-            statusText = 'Sesión de entrenamiento modificada exitosamente.'
-            utils.setPendingStatusMessage(statusType, [statusText]);
-
-            // Redirect to detail page of the training session
-            // and end event handler execution
-            window.location.assign('/historial/detalle.html?id=' + session.id);
-            return;
+              // Scroll to the top of the page
+              window.scrollTo({top: 0, behavior: 'smooth'});
+            });
           } else {
-            // If the training session was not updated, something went wrong
-            statusText = 'Error inesperado al tratar de modificar los datos.'
+            // If the form is not valid, show error message
+            utils.clearStatusMessages();
+            utils.addStatusMessage(
+                'alert-danger',
+                ['Corrija los errores en los datos ingresados.']
+            );
+
+            // Scroll to the top of the page
+            window.scrollTo({top: 0, behavior: 'smooth'});
           }
+
+          // Add .was-validated to form if it wasn't already
+          form.classList.add('was-validated');
+        });
+      })
+      .catch((error) => {
+        // Show appropriate error message
+        let statusText = '';
+
+        if (error === 'training-session-not-found') {
+          statusText =
+              'La sesión de entrenamiento solicitada no fue encontrada en su '
+              + 'historial de entrenamiento.';
+        } else if (error === 'exercise-items-not-found') {
+          statusText =
+              'La sesión de entrenamiento solicitada no contiene ejercicios. '
+              + 'Intente de nuevo, o elimine la sesión de entrenamiento desde '
+              + 'el historial y regístrela de nuevo si el problema persiste.';
+        } else if (error === 'exercise-items-count-does-not-match') {
+          statusText =
+              'Los datos de uno o varios ejercicios de la sesión de '
+              + 'entrenamiento no pudieron ser encontrados. Intente de nuevo, '
+              + 'o elimine la sesión de entrenamiento desde el historial y '
+              + 'regístrela de nuevo si el problema persiste.';
+        } else if (error === 'invalid-training-session') {
+          statusText = 'La sesión de entrenamiento contiene datos inválidos.';
+        } else if (error === 'deadline-exceeded') {
+          statusText =
+              'El tiempo de respuesta de la solicitud expiró. '
+              + 'Intente de nuevo más tarde.';
+        } else if (error === 'permission-denied') {
+          statusText = 'No tiene permiso de realizar la operación de consulta.';
+        } else if (error === 'unavailable') {
+          statusText =
+              'Servicio temporalmente no disponible. '
+              + 'Intente de nuevo más tarde';
         } else {
-          // If the form is not valid
-          statusText = 'Corrija los errores en los datos ingresados.';
+          statusText = `Error inesperado. Código: ${error}`;
         }
 
-        // Add .was-validated to form if it wasn't already
-        form.classList.add('was-validated');
-
-        // Clear status area and add appropriate error message
         utils.clearStatusMessages();
-        utils.addStatusMessage(statusType, [statusText]);
+        utils.addStatusMessage('alert-danger', [statusText]);
       });
-    }
+    } else {
+      // If the user is signed-out, add info message indicating the
+      // user to sign in
+      utils.addStatusMessage(
+        'alert-info',
+        ['Inicie sesión para gestionar sus sesiones de entrenamiento.']
+      );
 
-    // Add pending status message to page
-    utils.addPendingStatusMessage();
-  }
+      // Remove form if present
+      const form = document.getElementById(formId);
+      if (form) {
+        form.remove();
+      }
+      
+      // Reset main title
+      const mainTitle = document.querySelector('h1#main-title');
+      mainTitle.textContent = 'Modificar sesión de entrenamiento';
+
+      // Reset page title
+      document.title = 'Modificar sesión de entrenamiento ' + utils.NBSP
+          + ' 8A Training';
+
+      // Add empty results text if it isn't present
+      if (!document.querySelector('p#empty-text')) {
+        addEmptyFormText(container);
+      }
+    }
+  });
+
+  // Add pending status message to page
+  utils.addPendingStatusMessage();
 });
