05e3c3230fe26ad6f0084a1387a9963526ec5678
S8A <samuelochoap@gmail.com>
2022-04-03 15:43:58 -0400

    Fix error: getMonth() is index-0 for some reason

--- public/assets/js/utils.js
+++ public/assets/js/utils.js
@@ -624,7 +624,7 @@ export function createInvalidFeedbackElement() {
  */
 export function toISODateOnly(date) {
   const year = date.getFullYear();
-  const month = String(date.getMonth()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
   return `${year}-${month}-${day}`;
 }
