73ae6f7df4c35164bd49fc1de74490b3f0f525fc
S8A <samuelochoap@gmail.com>
2022-04-03 17:05:16 -0400

    Adapt delete confirm form JS to Firestore

--- public/assets/js/training-session-delete.js
+++ public/assets/js/training-session-delete.js
@@ -1,15 +1,23 @@
-import * as utils from '/assets/js/utils.js';
+import { TrainingSession, ExerciseItem, SetType } from './data-classes.js';
+import { auth, deleteTrainingSession, getTrainingSession } from './firebase.js';
+import * as utils from './utils.js';
 
 
-export function constructDeleteConfirmationPage(session) {
-  /* Construct delete confirmation page with the given training session data. */
-
+/**
+ * Construct delete confirmation page with the given training session data.
+ *
+ * @param {string} uid - UID of the signed-in user.
+ * @param {string} id - ID of the training session's document in Firestore.
+ * @param {TrainingSession} trainingSession - TrainingSession object.
+ */
+function constructDeleteConfirmationPage(uid, id, trainingSession) {
+  // Delete confirmation container
   const container = document.querySelector('#delete-confirmation');
 
   // Main title
   const mainTitle = container.querySelector('h1#main-title');
-  let fullTitle = utils.getTrainingSessionFullTitle(session);
-  let mainTitleText = 'Eliminar sesión de entrenamiento: ' + fullTitle;
+  const mainTitleText =
+      'Eliminar sesión de entrenamiento: ' + trainingSession.fullTitle;
   mainTitle.textContent = mainTitleText;
 
   // Page title
@@ -20,8 +28,9 @@ export function constructDeleteConfirmationPage(session) {
   container.removeChild(emptyText);
 
   // Question paragraph
-  const p = document.createElement('p');
-  p.textContent =
+  const question = document.createElement('p');
+  question.id = 'question';
+  question.textContent =
       '¿Está seguro de que desea eliminar el registro de esta sesión de '
       + 'entrenamiento? Esta acción es irreversible.';
 
@@ -38,19 +47,19 @@ export function constructDeleteConfirmationPage(session) {
 
   confirmButton.addEventListener('click', function () {
     // Try to delete the training session by its ID
-    let success = utils.deleteTrainingSession(session.id)
-
-    if (success) {
+    deleteTrainingSession(uid, id, trainingSession.exerciseItemsCount)
+    .then(() => {
       // If the training session is successfully deleted, set pending
       // success message and redirect to training log page
-      let text = 'Sesión de entrenamiento eliminada exitosamente.';
+      const text = 'Sesión de entrenamiento eliminada exitosamente.';
       utils.setPendingStatusMessage('alert-success', [text]);
       window.location.assign('/historial/');
-    } else {
+    })
+    .catch((error) => {
       // If deletion was unsuccessful, add error status message
-      let text = 'No se pudo eliminar la sesión de entrenamiento.';
+      const text = 'No se pudo eliminar la sesión de entrenamiento.';
       utils.addStatusMessage('alert-danger', [text]);
-    }
+    });
   });
 
   const cancelButton = document.createElement('button');
@@ -61,56 +70,130 @@ export function constructDeleteConfirmationPage(session) {
 
   cancelButton.addEventListener('click', function () {
     // If the user cancels the deletion, redirect to detail page
-    window.location.assign('/historial/detalle.html?id=' + session.id);
+    window.location.assign('/historial/detalle.html?id=' + id);
   });
 
   actionButtons.appendChild(confirmButton);
   actionButtons.appendChild(cancelButton);
 
   // Put everything together
-  container.appendChild(p);
+  container.appendChild(question);
   container.appendChild(actionButtons);
 }
 
 
+/**
+ * Add empty page text to the given container.
+ * @param {HTMLElement} container - Container to which the text will be added.
+ */
+ function addEmptyPageText(container) {
+  const emptyText = document.createElement('p');
+  emptyText.id = 'empty-text';
+  emptyText.textContent =
+      'No se ha podido cargar la información de la sesión de entrenamiento '
+      + 'solicitada.';
+  container.appendChild(emptyText);
+}
+
+
 window.addEventListener('load', function () {
-  let signedInAccount = utils.getSignedInAccount();
+  // Add pending status message to page
+  utils.addPendingStatusMessage();
 
-  if (!signedInAccount) {
-    // If not signed-in, set pending info message and redirect to sign-in
-    let text = 'Inicie sesión para gestionar sus sesiones de entrenamiento.';
-    utils.setPendingStatusMessage('alert-info', [text]);
-    window.location.assign('/iniciar-sesion.html?next=/historial/');
+  // Get query parameters
+  const params = utils.getQueryParams();
+
+  // Training session ID
+  const id = params.id;
+
+  if (!id) {
+    // If ID parameter is not set, redirect to training log page
+    window.location.assign('/historial/');
     return;
-  } else {
-    // If signed-in, set up signed-in header
-    utils.setUpSignedInHeader(signedInAccount);
-
-    // Get query params
-    const params = utils.getQueryParams();
-
-    // Training session ID
-    const id = Number(params.id);
-
-    // If no valid ID parameter set, redirect to training log page
-    if (!Number.isInteger(id) || id <= 0) {
-      window.location.assign('/historial/');
-      return;
-    }
-
-    // Try to get training session with the given ID
-    let session = utils.getTrainingSession(id);
-
-    if (!session) {
-      // If the training session was not found, add error status message
-      let text = 'La sesión de entrenamiento solicitada no existe o '
-      + 'no pertenece a su cuenta.';
-      utils.clearStatusMessages();
-      utils.addStatusMessage('alert-danger', [text]);
-    } else {
-      // If the training session was found, construct delete confirmation
-      // page for the training session
-      constructDeleteConfirmationPage(session);
-    }
   }
+
+  // Set up authentication state observer
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      // If user is signed in, set up signed-in header
+      utils.setUpSignedInHeader(user);
+
+      // Try to get the user's requested training session
+      getTrainingSession(user.uid, id)
+      .then((trainingSession) => {
+        // Construct the returned training session's detail page
+        constructDeleteConfirmationPage(user.uid, id, trainingSession);
+      })
+      .catch((error) => {
+        // Show appropriate error message
+        let statusText = '';
+
+        if (error === 'training-session-not-found') {
+          statusText =
+              'La sesión de entrenamiento solicitada no fue encontrada en su '
+              + 'historial de entrenamiento.';
+        } else if (error === 'exercise-items-not-found') {
+          statusText =
+              'La sesión de entrenamiento solicitada no contiene ejercicios. '
+              + 'Intente de nuevo, o elimine la sesión de entrenamiento desde '
+              + 'el historial y regístrela de nuevo si el problema persiste.';
+        } else if (error === 'exercise-items-count-does-not-match') {
+          statusText =
+              'Los datos de uno o varios ejercicios de la sesión de '
+              + 'entrenamiento no pudieron ser encontrados. Intente de nuevo, '
+              + 'o elimine la sesión de entrenamiento desde el historial y '
+              + 'regístrela de nuevo si el problema persiste.';
+        } else if (error === 'invalid-training-session') {
+          statusText = 'La sesión de entrenamiento contiene datos inválidos.';
+        } else if (error === 'deadline-exceeded') {
+          statusText =
+              'El tiempo de respuesta de la solicitud expiró. '
+              + 'Intente de nuevo más tarde.';
+        } else if (error === 'permission-denied') {
+          statusText = 'No tiene permiso de realizar la operación de consulta.';
+        } else if (error === 'unavailable') {
+          statusText =
+              'Servicio temporalmente no disponible. '
+              + 'Intente de nuevo más tarde';
+        } else {
+          statusText = `Error inesperado. Código: ${error}`;
+        }
+
+        utils.clearStatusMessages();
+        utils.addStatusMessage('alert-danger', [statusText]);
+      });
+    } else {
+      // If the user is signed-out, add info message indicating the
+      // user to sign in
+      utils.addStatusMessage(
+        'alert-info',
+        ['Inicie sesión para gestionar sus sesiones de entrenamiento.']
+      );
+
+      // Remove question and action buttons
+      const container = document.querySelector('#delete-confirmation');
+
+      const question = container.querySelector('p#question');
+      question.remove();
+
+      const actionButtons = container.querySelector('#action-buttons');
+      actionButtons.remove();
+
+      // Reset main title
+      const mainTitle = container.querySelector('h1#main-title');
+      mainTitle.textContent = 'Eliminar sesión de entrenamiento';
+
+      // Reset page title
+      document.title = 'Eliminar sesión de entrenamiento ' + utils.NBSP
+          + ' 8A Training';
+
+      // Add empty page text if it isn't present
+      if (!document.querySelector('p#empty-text')) {
+        addEmptyPageText(container);
+      }
+    }
+  });
+
+  // Add pending status message to page
+  utils.addPendingStatusMessage();
 });
